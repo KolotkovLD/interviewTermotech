6b935ac50ed9a6027e3eabdd40ddc0eff10754d5
KolotkovLD <kolotkov93@gmail.com>
2018-05-06 23:25:43 +0300

    change third

--- test_third.py
+++ test_third.py
@@ -1,32 +1,24 @@
 import datetime as dt
-from dateutil.relativedelta import relativedelta
-
-def yearsago(years, from_date=None):
-    if from_date is None:
-        from_date = dt.datetime.now()
-    return from_date - relativedelta(years=years)
 
 def checkPasport(birth, regist):
-    delta = regist - birth
     years_delta = regist.year - birth.year
-    check_bool = False
+    check = False
     for i in (14, 20, 45):
         if years_delta == i:
+            # acceptable time for registration
             start_regist = dt.datetime(birth.year + i, birth.month, birth.day)
             end_regist = dt.datetime(birth.year + i, birth.month + 1, birth.day)
-            print("start_regist = ", start_regist)
+
+            # deltas
             limit_s = regist - start_regist
             limit_e = end_regist - regist
-            print("limit_s = ", limit_s)
-            print("limit_e = ", limit_e)
+
             if limit_s.days >= 0 and limit_e.days >= 0:
-                check_bool = True
-    return check_bool
+                check = True
+    return check
 
-
+# YYYY, M, D
 birth = dt.datetime(2000, 5, 3)
+registration = dt.datetime(2014, 6, 5)
 
-registration = dt.datetime(2015, 6, 3)
-
-print(checkPasport(birth, registration))
-#print(str(yearsago(21, birth)))
+checkPasport(birth, registration)
