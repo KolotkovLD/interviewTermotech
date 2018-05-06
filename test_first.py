3ca648865ba7f8d696717a6bf89c6e3f033c8e76
KolotkovLD <kolotkov93@gmail.com>
2018-05-06 23:30:35 +0300

    change first

--- test_first.py
+++ test_first.py
@@ -1,13 +1,11 @@
-
 def combine(keys, values):
     key_len = len(keys)
     value_len = len(values)
     if key_len > value_len:
         for i in range(key_len-value_len):values.append(None)
-    my_dict = dict(zip(keys, values))
-    return my_dict
+    return dict(zip(keys, values))
 
 
 key_lst = ['key1', 'key2', 'key3', 'key4', 'key5', 'key6']
 value_lst = ['value1', 'value2', 'value3']
-print(combine(key_lst, value_lst))
+new_dict = combine(key_lst, value_lst)
